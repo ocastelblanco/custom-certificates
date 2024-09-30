9b5f2d056c1de6f545b11661d22f56d83ea15dd2
Oliver Castelblanco <ocastelblanco@gmail.com>
2024-09-30 18:01:11 -0500

    fix: errores varios

--- src/cursos/cursos.php
+++ src/cursos/cursos.php
@@ -1,16 +1,33 @@
 <?php
-
 $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 if ($contentType === "application/json") {
-  $crudo = trim(file_get_contents("php://input"));
-  $cursos = json_decode($crudo, true);
-  if ($cursos) {
-    $salida = [
-      "error" => null,
-      "cursos" => $cursos
-    ];
-    print json_encode($salida);
+  if (isset($_GET) && isset($_GET["url"])) {
+    $url = $_GET["url"];
+    $url = "../assets/data/cursos.json";
+    $crudo = trim(file_get_contents("php://input"));
+    $cursos = json_decode($crudo, true);
+    if (
+      $cursos &&
+      count($cursos) > 0 &&
+      $cursos[0] &&
+      $cursos[0]["id"]
+    ) {
+      if (file_put_contents($url, $crudo)) {
+        $salida = [
+          "error" => null,
+          "cursos" => $cursos,
+          "url" => $url
+        ];
+        print json_encode($salida);
+      } else {
+        print '{"error": "No se pudo escribir en el JSON"}';
+      }
+    } else {
+      print '{"error": "No se pudo decodificar el JSON"}';
+    }
   } else {
-    print '{"error": "No se pudo decodificar el JSON"}';
+    print '{"error": "No se envió la URL del JSON destino"}';
   }
+} else {
+  print '{"error": "La información enviada no tiene el formato correcto"}';
 }
